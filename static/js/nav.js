e7513c4dbc0915b2056e5154d86a4f6e7b9aef5b
Ryan Rogalski <ryan.rogalski@gmail.com>
2012-05-01 01:43:33 -0400

    view all working and tips working

--- static/js/nav.js
+++ static/js/nav.js
@@ -2,20 +2,32 @@ $(document).ready(function() {
   $('button.new_comment').click(function(event) {
     $('#comments').slideDown(400);
   });
-  $('.tip_options a.selected').hover(function() {
-    $(this).text('Cancel This?');
-  }, function() {
-    $(this).text('You already do this!');
-  });
-  $('.tip_options a.selected').click(function(event) {
+  $('a.selected').live("click", function(event) {
     $(this).fadeOut(400).parent().html('<a class="already">I already do this</a><a class="want">I want to do this</a><a class="apply">Doesn\'t apply to me</a>');
   });
-  $('a.want').click(function(event) {
+  $('a.want').live("click", function(event) {
     event.preventDefault();
     $(this).parent().fadeOut(400, function(){
       $(this).html('<a class="selected">Added this to your plan</a>').fadeIn(400);
     });
-  });  
+  });
+  $('a.apply').live("click", function(event) {
+    event.preventDefault();
+    $(this).parent().fadeOut(400, function(){
+      $(this).html('<a class="selected">This tip will be ignored</a>').fadeIn(400);
+    });
+  });
+  $('a.already').live("click", function(event) {
+    event.preventDefault();
+    $(this).parent().fadeOut(400, function(){
+      $(this).html('<a class="selected">Keep up the good work!</a>').fadeIn(400);
+    });
+  });
+  $('a.view_all').toggle(function() {
+    $('ul.badges').animate({'height': '800px'}, 2000)
+  }, function() {
+    $('ul.badges').animate({'height': '210px'}, 2000)
+  });
 });
 
 

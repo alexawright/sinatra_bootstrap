75765359f7acf545d8cbc5a45edd043d5f62602f
Ryan Rogalski <ryan.rogalski@gmail.com>
2012-05-01 03:47:31 -0400

    cute lil animation

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $('button.new_comment').click(function(event) {
-    $('#comments').slideDown(400);
+    $('#comments').slideDown(400, function(){
+      $('#comments .entry').animate({'opacity': 1}, 400, function() {
+        $('.no_comment').slideUp(400);
+      })
+    });
   });
   $('a.selected').live("click", function(event) {
     $(this).fadeOut(400).parent().html('<a class="already">I already do this</a><a class="want">I want to do this</a><a class="apply">Doesn\'t apply to me</a>');
